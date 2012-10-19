a534a2400c244e75eceda5f8f1475013fb260d8a
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-10-19 11:07:13 +0900

    improved the form validation & auto completion

--- src/main/java/org/jenkinsci/plugins/recipe/ingredients/JobIngredient.java
+++ src/main/java/org/jenkinsci/plugins/recipe/ingredients/JobIngredient.java
@@ -2,7 +2,13 @@ package org.jenkinsci.plugins.recipe.ingredients;
 
 import com.thoughtworks.xstream.io.xml.XppDriver;
 import hudson.Extension;
+import hudson.model.AbstractProject;
+import hudson.model.AutoCompletionCandidates;
+import hudson.model.Item;
+import hudson.model.ItemGroup;
 import hudson.model.Job;
+import hudson.model.TopLevelItem;
+import hudson.util.FormValidation;
 import jenkins.model.Jenkins;
 import jenkins.util.xstream.XStreamDOM;
 import jenkins.util.xstream.XStreamDOM.ConverterImpl;
@@ -11,6 +17,9 @@ import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.jenkinsci.plugins.recipe.Ingredient;
 import org.jenkinsci.plugins.recipe.IngredientDescriptor;
 import org.jenkinsci.plugins.recipe.Recipe;
+import org.kohsuke.stapler.AncestorInPath;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
 import java.io.ByteArrayInputStream;
@@ -26,6 +35,15 @@ public class JobIngredient extends Ingredient {
     public JobIngredient(String name, XStreamDOM definition) {
         this.name = name;
         this.definition = definition;
+    }
+
+    @DataBoundConstructor
+    public JobIngredient(String name) {
+        this.name = name;
+        AbstractProject i = Jenkins.getInstance().getItemByFullName(name, AbstractProject.class);
+        if (i==null)
+            throw new IllegalArgumentException("No such job: "+name);
+        this.definition = XStreamDOM.from(i.getConfigFile().getXStream(),i);
     }
 
     public void setName(String name) {
@@ -62,6 +80,16 @@ public class JobIngredient extends Ingredient {
         @Override
         public String getDisplayName() {
             return "Job";
+        }
+
+        public AutoCompletionCandidates doAutoCompleteChildProjects(@QueryParameter String value) {
+            return AutoCompletionCandidates.ofJobNames(AbstractProject.class,value,null,Jenkins.getInstance());
+        }
+
+        public FormValidation doCheckName(@QueryParameter String name) {
+            AbstractProject i = Jenkins.getInstance().getItemByFullName(name, AbstractProject.class);
+            if (i==null)    return FormValidation.error("No such job: "+name);
+            return FormValidation.ok();
         }
     }
 }
