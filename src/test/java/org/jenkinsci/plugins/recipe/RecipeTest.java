914c8eaa61ee1ec25913f1a6fa32dc1e90955acd
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-12-29 07:41:17 -0800

    added a new upload mechanism that shares with http://recipe.jenkins-ci.org/

--- src/test/java/org/jenkinsci/plugins/recipe/RecipeTest.java
+++ src/test/java/org/jenkinsci/plugins/recipe/RecipeTest.java
@@ -16,7 +16,7 @@ public class RecipeTest extends TestCase {
         xs.alias("recipe", Recipe.class);
         xs.alias("param", Parameter.class);
 
-        Recipe r = new Recipe("1.0","foo","lorem ipsum", Arrays.<Ingredient>asList(
+        Recipe r = new Recipe("foo","1.0","foo","lorem ipsum", Arrays.<Ingredient>asList(
                 new Parameter("foo"),
                 new Parameter("bar")
         ));
