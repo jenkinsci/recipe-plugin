08436784d164c3a3607c4798cebd5c51ab055344
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-12-30 13:09:24 -0800

    hover effect is harmful especially on the advanced page

--- src/main/resources/org/jenkinsci/plugins/recipe/ImportWizard/tabBar.groovy
+++ src/main/resources/org/jenkinsci/plugins/recipe/ImportWizard/tabBar.groovy
@@ -2,6 +2,12 @@ package org.jenkinsci.plugins.recipe.ImportWizard;
 
 def l = namespace(lib.LayoutTagLib)
 
+style """
+.bigtable tr:hover {
+  background-color: #FFFFFF !important;
+}
+
+"""
 l.tabBar {
     l.tab(name:_("Available"), active:page=="available", href:".")
     l.tab(name:_("Advanced"), active:page=="advanced", href:"advanced")
